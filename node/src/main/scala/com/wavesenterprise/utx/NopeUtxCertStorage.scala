fd7de59d8be8a3782f0495b72d5552aa843ae33a
Gamzaliev Ruslan <rgamzaliev@wavesplatform.com>
2022-11-14 14:37:25 +0400

    Fixed formatting

--- node/src/main/scala/com/wavesenterprise/utx/NopeUtxCertStorage.scala
+++ node/src/main/scala/com/wavesenterprise/utx/NopeUtxCertStorage.scala
@@ -6,6 +6,6 @@ import com.wavesenterprise.transaction.{Transaction, ValidationError}
 
 trait NopeUtxCertStorage extends UtxCertStorage {
   def putCertChain(tx: Transaction, certChain: CertChain): Either[ValidationError, Unit] = Right(())
-  def removeCertChain(tx: Transaction): Unit         = ()
-  def getCertChain(txId: ByteStr): Option[CertChain] = None
+  def removeCertChain(tx: Transaction): Unit                                             = ()
+  def getCertChain(txId: ByteStr): Option[CertChain]                                     = None
 }
