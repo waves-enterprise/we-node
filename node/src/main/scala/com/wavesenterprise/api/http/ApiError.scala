49c0453a22b26f39740f8078a0d885b0ef7a683b
Artemiy Pospelov <apospelov@wavesenterprise.com>
2022-11-21 18:19:33 +0000

    WE-8424 Auth default case

--- node/src/main/scala/com/wavesenterprise/api/http/ApiError.scala
+++ node/src/main/scala/com/wavesenterprise/api/http/ApiError.scala
@@ -189,12 +189,6 @@ object ApiError extends IntEnum[ApiError] {
     override val code: StatusCode = StatusCodes.BadRequest
   }
 
-  case object IllegalAuthType extends ApiError {
-    override val value: Int       = 11
-    override val message: String  = "Illegal authorization type"
-    override val code: StatusCode = StatusCodes.BadRequest
-  }
-
   //VALIDATION
   case object InvalidSignature extends ApiError {
     override val value   = 101
