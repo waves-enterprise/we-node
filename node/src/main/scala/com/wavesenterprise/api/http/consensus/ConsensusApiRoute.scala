47fee2ab182484edacebc8c61501ac7b6f03ceb4
Evgeniy Gnatovskiy <egnatovskiy@web3tech.ru>
2023-09-28 01:18:37 +0700

    WE-8519: edit consensus/minersAtHeight/{height} in ConsensusApiRoute

--- node/src/main/scala/com/wavesenterprise/api/http/consensus/ConsensusApiRoute.scala
+++ node/src/main/scala/com/wavesenterprise/api/http/consensus/ConsensusApiRoute.scala
@@ -2,6 +2,7 @@ package com.wavesenterprise.api.http.consensus
 
 import akka.http.scaladsl.server.Route
 import com.wavesenterprise.account.Address
+import com.wavesenterprise.api.ValidInt._
 import com.wavesenterprise.api.http._
 import com.wavesenterprise.api.http.auth.ApiProtectionLevel.ApiKeyProtection
 import com.wavesenterprise.api.http.auth.AuthRole.Administrator
@@ -169,17 +170,20 @@ class ConsensusApiRoute(val settings: ApiSettings,
     *
     * Retrieves list of miners for timestamp of a block at given height
     **/
-  def minersAtHeight: Route = (path("minersAtHeight" / IntNumber) & get) { height =>
-    withExecutionContext(scheduler) {
-      complete {
-        for {
-          blockHeader <- blockchain.blockHeaderAt(height)
-          requestedTimestamp = blockHeader.timestamp
-          minerAddresses     = blockchain.miners.currentMinersSet(requestedTimestamp).map(_.toString)
-        } yield MinersAtHeight(minerAddresses.toSeq, height)
-      }
+  def minersAtHeight: Route = (path("minersAtHeight" / IntNumber) & get)(height => {
+    PositiveInt(height).processRoute {
+      height =>
+        withExecutionContext(scheduler) {
+          complete {
+            for {
+              blockHeader <- blockchain.blockHeaderAt(height)
+              requestedTimestamp = blockHeader.timestamp
+              minerAddresses     = blockchain.miners.currentMinersSet(requestedTimestamp).map(_.toString)
+            } yield MinersAtHeight(minerAddresses.toSeq, height)
+          }
+        }
     }
-  }
+  })
 
   /**
     * GET /consensus/miners/{timestamp}
